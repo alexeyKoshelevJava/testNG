5bb42ce4ae264aec40929329db3e0412f74ebdf7
Alexey Koshelev <koshelev2011@mail.ru>
2021-12-18 01:22:17 +0300

    version11

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,9 +7,7 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("For input string: введи оператор\n" +
                 "введи первое число\n" +
-                "введи второе число\n" +
-                "5.0\n" +
-                "5.0\n");
+                "введи второе число\n");
         String operator = in.nextLine();
         String first = in.nextLine();
         String two = in.nextLine();
